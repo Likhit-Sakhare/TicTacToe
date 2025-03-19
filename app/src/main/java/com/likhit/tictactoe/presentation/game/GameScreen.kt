f0467e8d19f78a9fc8f84423df4e0726c8bf7809
Likhit Sakhare <sakhare1181likhit@gmail.com>
2025-03-19 15:26:49 +0530

    Added a score card

--- app/src/main/java/com/likhit/tictactoe/presentation/game/GameScreen.kt
+++ app/src/main/java/com/likhit/tictactoe/presentation/game/GameScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -21,21 +23,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.likhit.tictactoe.R
 import com.likhit.tictactoe.presentation.game.components.CirclePiece
@@ -45,6 +55,7 @@ import com.likhit.tictactoe.presentation.game.components.GameBoardBase
 import com.likhit.tictactoe.presentation.game.utils.BoardCellValue
 import com.likhit.tictactoe.ui.theme.CustomBlue
 import com.likhit.tictactoe.ui.theme.GrayBackground
+import com.likhit.tictactoe.ui.theme.GreenishYellow
 
 @Composable
 fun GameScreen(
@@ -58,6 +69,10 @@ fun GameScreen(
     val victoryType = viewModel.victoryType
     val hasWon = viewModel.hasWon
 
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -68,24 +83,35 @@ fun GameScreen(
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.End,
             verticalAlignment = Alignment.Top
         ) {
-            Text(
-                text = "Player 'O': $playerCircleCount",
-                fontSize = 16.sp,
-                color = Color.Black
-            )
-            Text(
-                text = "Draw: $drawCount",
-                fontSize = 16.sp,
-                color = Color.Black
-            )
-            Text(
-                text = "Player 'X': $playerCrossCount",
-                fontSize = 16.sp,
-                color = Color.Black
-            )
+            Box(
+                modifier = Modifier
+                    .size(42.dp)
+                    .border(
+                        width = 1.5.dp,
+                        color = Color.Black.copy(alpha = 0.5f),
+                        RoundedCornerShape(8.dp)
+                    )
+                    .shadow(
+                        elevation = 16.dp,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            ){
+                IconButton(
+                    onClick = {
+                        showDialog = !showDialog
+                    }
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.score),
+                        contentDescription = "Score Card"
+                    )
+                }
+            }
+
+
         }
 
         Spacer(modifier = Modifier.weight(1f))
@@ -216,6 +242,53 @@ fun GameScreen(
                             fontSize = 16.sp
                         )
                     }
+                }
+            }
+        }
+    }
+
+    if(showDialog){
+        Dialog(
+            onDismissRequest = {
+                showDialog = false
+            }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White)
+                    .padding(20.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Player O: $playerCircleCount",
+                        fontSize = 24.sp,
+                        color = GreenishYellow,
+                        fontFamily = FontFamily(
+                            Font(R.font.dancing_script_regular, FontWeight.Bold)
+                        )
+                    )
+                    Text(
+                        text = "Draw: $drawCount",
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        fontFamily = FontFamily(
+                            Font(R.font.dancing_script_regular, FontWeight.Bold)
+                        )
+                    )
+                    Text(
+                        text = "Player X: $playerCrossCount",
+                        fontSize = 24.sp,
+                        color = CustomBlue,
+                        fontFamily = FontFamily(
+                            Font(R.font.dancing_script_regular, FontWeight.Bold)
+                        )
+                    )
                 }
             }
         }
